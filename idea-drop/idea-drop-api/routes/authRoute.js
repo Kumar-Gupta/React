a8f007ce94e1d253cab9028a5e895afb7325910f
Kumar Manglam Gupta <kumargupta24102000@gmail.com>
2026-01-17 00:31:47 +0530

    Updates

--- idea-drop/idea-drop-api/routes/authRoute.js
+++ idea-drop/idea-drop-api/routes/authRoute.js
@@ -36,7 +36,7 @@ router.post('/register', async (req, res, next)=> {
     res.cookie('refreshToken', refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'none',
+        sameSite: process.env.NODE_ENV ==='production' ? 'none' : 'lax',
         maxAge: 30 * 24 * 60 * 60 * 1000 //30Days
     })
 
@@ -90,7 +90,7 @@ router.post('/login', async (req, res, next) => {
         res.cookie('refreshToken', refreshToken, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'none',
+            sameSite: process.env.NODE_ENV ==='production' ? 'none' : 'lax',
             maxAge: 30 * 24 * 60 * 60 * 1000 //30Days
         })
 
@@ -120,7 +120,7 @@ router.post('/logout', async (req, res, next)=> {
     res.clearCookie('refreshToken', {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'none',
+        sameSite: process.env.NODE_ENV ==='production' ? 'none' : 'lax',
     })
 
     res.status(200).json({ message : "User Logged out successfully"})
---
+++ idea-drop/idea-drop-ui/src/context/AuthContext.tsx
@@ -0,0 +1,42 @@
+import { createContext, useContext, useEffect, useState, type ReactNode } from "react";
+import { refreshAccessToken } from "@/api/auth";
+
+type AuthContextType = {
+    accessToken: string | null;
+    setAccessToken : (string: string | null ) => void;
+    user: {id: string, name : string, email: string } | null;
+    setUser: (user: AuthContextType['user']) => void
+ }
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined)
+
+export const AuthProvider = ({children} : {children : ReactNode}) => {
+    const [accessToken, setAccessToken] = useState<string | null>(null);
+    const [user, setUser] = useState<AuthContextType['user'] | null>(null);
+
+    useEffect(()=>{
+        const loadAuth = async () => {
+            try {
+                const {accessToken: newToken, user} =await refreshAccessToken();
+                setAccessToken(newToken);
+                setUser(user);
+            } catch (err: any) {
+                console.log('Failed to get refresh access token', err)
+            }
+        }
+
+        loadAuth();
+    },[])
+
+    return(
+        <AuthContext.Provider value={{accessToken, setAccessToken, user, setUser }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export const useAuth = () =>{
+    const context = useContext(AuthContext);
+    if(!context) throw new Error('useAuth must be used within a provide');
+    return context;
+}
